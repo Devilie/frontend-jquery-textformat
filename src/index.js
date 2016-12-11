dc424fa0ddef998c29c1b88adbf5f0f31dfcfc81
Devilie <devilie1989@gmail.com>
2016-12-11 16:57:28 +0200

    Formatting during text typing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log($)
+console.log($);
+
+function format_massage()
+{
+    var text_area = $('.message-field');
+    var output = $('.message-formatted');
+    var temp_str;
+    
+    function formated()
+    {
+        temp_str = text_area.val()
+                            .trim()
+                            .replace(/\s+/g," ")
+                            .replace(/\n+/g," ")
+                            .toLocaleLowerCase();
+        output.text(temp_str);
+    }
+    
+    text_area.keyup(formated);
+    
+}
+
+$(document).ready(format_massage);
