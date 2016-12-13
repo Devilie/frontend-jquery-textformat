6c6982bd1e666895111a5b4bcbfe1bf753e33afa
Devilie <devilie1989@gmail.com>
2016-12-13 19:19:57 +0200

    Delete some unused code.

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@
 
 console.log($);
 
-function format_massage()
+var text_area = $('.message-field');
+var output = $('.message-formatted'); 
+    
+function formated()
 {
-    var text_area = $('.message-field');
-    var output = $('.message-formatted');
-    
-    function formated()
-    {
-        var temp_str = text_area.val()
-                            .trim()
-                            .replace(/\s+/g," ")
-                            .replace(/\n+/g," ")
-                            .toLocaleLowerCase();
-        output.text(temp_str);
-    }
-    
-    text_area.keyup(formated);
-    
+    var temp_str = text_area.val()
+                        .trim()
+                        .replace(/\s+/g," ")
+                        .replace(/\n+/g," ")
+                        .toLocaleLowerCase();
+    output.text(temp_str);
 }
-$('.message-field').on('input',format_massage);
-//$(document).ready(format_massage);
+
+text_area.on('input',formated);
